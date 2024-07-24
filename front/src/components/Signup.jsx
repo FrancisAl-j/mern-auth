d94ca84c1e6f155277196308fbb75a60a3478d4d
Francis <kikobilas123@gmail.com>
2024-07-25 01:07:48 +0800

    Unfinished UI for sign up form

--- front/src/components/Signup.jsx
+++ front/src/components/Signup.jsx
@@ -1,7 +1,37 @@
 const Signup = () => {
     return (
-        <div>
-            <h1>Sign up page</h1>
+        <div className="form-wrapper">
+            <h1>Sign up</h1>
+            
+            <div className="form-container">
+                <form>
+                    <div className="form-elements">
+                        <label>Username</label>
+                        <input type="text" 
+                               name="username" 
+                               id="username"
+                        />
+                    </div>
+
+                    <div className="form-elements">
+                        <label>Email</label>
+                        <input type="email" 
+                               name="email" 
+                               id="email"
+                        />
+                    </div>
+
+                    <div className="form-elements">
+                        <label>Password</label>
+                        <input type="password" 
+                               name="password" 
+                               id="password"
+                        />
+                    </div>
+
+                    <button type="submit">Sign up</button>
+                </form>
+            </div>
         </div>
     )
 }
