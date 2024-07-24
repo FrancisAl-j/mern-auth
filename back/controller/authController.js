48ca18d08fcbc0c39009b2fb24d1da0c325686dc
Francis <kikobilas123@gmail.com>
2024-07-24 22:53:38 +0800

    Encrypt password and finished the authentication

--- back/controller/authController.js
+++ back/controller/authController.js
@@ -1,11 +1,13 @@
 import User from '../models/userModel.js';
+import bcryptjs from 'bcryptjs' // For encrypting the password inside database for security
 
 const signup = async (req, res) => {
     const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10); // And this is how to use it
     const newUser = new User({
         username,
         email,
-        password
+        password: hashedPassword
     })
 
     try {
