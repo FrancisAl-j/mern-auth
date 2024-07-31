4904dcc96b59c6125457a8cc7cde0fea4df4e5df
Francis <kikobilas123@gmail.com>
2024-07-31 19:02:50 +0800

    Completes profile UI

--- front/src/components/Profile.jsx
+++ front/src/components/Profile.jsx
@@ -1,5 +1,34 @@
+import { useSelector } from "react-redux";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <div className="profile-container">
+      <h1>Profile</h1>
+      <form>
+        <img src={currentUser.profilePicture} alt="Profile" />
+        <div className="body-container">
+          <input
+            type="text"
+            defaultValue={currentUser.username}
+            placeholder="Username"
+          />
+          <input
+            type="email"
+            defaultValue={currentUser.email}
+            placeholder="Email"
+          />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button className="update-btn">Update</button>
+      </form>
+      <div className="btns">
+        <span>Delete Account</span>
+        <span>Sign out</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
