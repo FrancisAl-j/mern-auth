dfa1f7c23fc8e7e79301cffbcd30c59653171e2c
Francis <kikobilas123@gmail.com>
2024-08-03 01:08:59 +0800

    Finished UI of home

--- back/utils/verifyUser.js
+++ back/utils/verifyUser.js
@@ -4,17 +4,14 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
-  console.log(token);
 
   if (!token) {
-    console.log("No token found in cookies"); // Debugging line
     return next(errorHandler(401, "You are not authenticated!"));
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(401, "Token is not valid"));
 
-    console.log("Decoded user:", user); // Debugging line
     req.user = user;
     next();
   });
--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,9 +1,38 @@
-const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+import { useSelector } from "react-redux";
+import DefaultProfile from "../assets/profile.jpg";
+import SignOut from "../assets/logout.svg";
 
-export default Home
+const Home = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <div className="home-container">
+      <div className="pfp-container">
+        <img
+          className="pfp"
+          src={currentUser ? currentUser.profilePicture : DefaultProfile}
+          alt="profile"
+        />
+        <p>
+          {currentUser
+            ? currentUser.username
+            : `Guest${Math.floor(Math.random() * 999)}`}
+        </p>
+        <img className="logout" src={SignOut} alt="" />
+      </div>
+      <div className="content-container">
+        <h1>Welcome to my Mern-Authentication App</h1>
+        <p>
+          This Web-App is a full-stack build using mern development, it has the
+          fundamentals of CRUD operation. It has a sign up page where you can
+          create your own account, sign in page to sign in your accout, this
+          web-app also use google account to sign up and sign in to this
+          website. Had an authentication to authenticate the user using
+          cookie/token.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
