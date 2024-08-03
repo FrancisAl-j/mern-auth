539d53913825a42ad61ed0756425aecf172c725b
Francis <kikobilas123@gmail.com>
2024-08-03 22:31:08 +0800

    Finished product

--- front/src/components/About.jsx
+++ front/src/components/About.jsx
@@ -5,6 +5,11 @@ const About = () => {
     <div className="about-container">
       <div className="content-container">
         <h1>About</h1>
+        <h2>Developer:</h2>
+        <h3>Name:</h3>
+        <p>Francis Al-j D. Bilas</p>
+        <h3>Age:</h3>
+        <p>22 years old</p>
       </div>
     </div>
   );
