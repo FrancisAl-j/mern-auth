6f0658f6642c74fb73b98decda44d2d2085ff4a6
Francis <kikobilas123@gmail.com>
2024-08-04 17:25:02 +0800

    added a media query for different screen sizes

--- front/src/components/nav.jsx
+++ front/src/components/nav.jsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Menu from "../assets/menu.svg";
+import { useState } from "react";
 
 const Nav = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [show, setShow] = useState(false);
+
+  const handleMenu = () => {
+    setShow((prevShow) => !prevShow);
+  };
   return (
     <nav>
       <Link to="/">
         <h1 className="logo">Auth App</h1>
       </Link>
 
-      <ul>
+      <ul className={show ? "show-menu" : ""}>
         <Link to="/">
           <li>Home</li>
         </Link>
@@ -32,6 +39,9 @@ const Nav = () => {
           )}
         </Link>
       </ul>
+      <div className="menu">
+        <img onClick={handleMenu} src={Menu} alt="menu" />
+      </div>
     </nav>
   );
 };
